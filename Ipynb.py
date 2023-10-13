4a4510cda271069f594a702d2004adc7cc596eae
Jack <37254717+jackbullen@users.noreply.github.com>
2023-10-13 00:10:15 -0700

    Update Ipynb.py

--- Ipynb.py
+++ Ipynb.py
@@ -19,9 +19,3 @@ class Ipynb:
 
         with open(self.path, 'w') as f:
             f.write(json.dumps(saved))
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.name
